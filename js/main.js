ac3a7c2dcc837143279f2fd8b54bd99df31d6889
ShohruxMurodovich <murodovichshohruh@gmail.com>
2022-03-04 21:50:35 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".form__input");
+var elList = document.querySelector(".list");
+
+var toDoList = [];
+
+elForm.addEventListener("submit" , function(evt){
+  evt.preventDefault()
+
+  var inputVal = elInput.value.trim();
+
+  var toDo = {
+    id: toDoList.length,
+    title: inputVal,
+  };
+
+  toDoList.push(toDo.title);
+  elList.innerHTML = "";
+
+  for (var item of toDoList) {
+
+    var li = document.createElement("li");
+
+    li.textContent = item;
+
+    elList.appendChild(li);
+
+    elInput.value = ""
+  }
+});
